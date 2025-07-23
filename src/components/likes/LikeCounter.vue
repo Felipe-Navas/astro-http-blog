7f7e4bcdaac8e31e3301aa2752624f30eff2fe32
Felipe Navas <felipenavas.itec@gmail.com>
2025-07-23 19:13:13 -0300

    Add debounce and PUT call

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -6,13 +6,12 @@
   <button v-else @click="likePost">
     Likes <span>{{ likeCount }}</span>
   </button>
-
-  {{ likeClicks }}
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import confetti from 'canvas-confetti'
+import debounce from 'lodash.debounce'
 
 interface Props {
   postId: string
@@ -23,6 +22,22 @@ const props = defineProps<Props>()
 const likeCount = ref(0)
 const likeClicks = ref(0)
 const isLoading = ref(true)
+
+watch(
+  likeCount,
+  debounce(() => {
+    console.log('likeClicks.value', likeClicks.value)
+    fetch(`/api/posts/likes/${props.postId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ likes: likeClicks.value }),
+    })
+
+    likeClicks.value = 0
+  }, 500)
+)
 
 const likePost = () => {
   likeCount.value++
